e8c36cc85a8e32aea850f3bc5d1a9de91ca02b7a
WuDiDaBinGe <1151680016@qq.com>
2021-02-20 14:04:53 +0800

    完成了第一次出题方阵计算

--- CollegeComputing/FMatrix/FMatrix.py
+++ CollegeComputing/FMatrix/FMatrix.py
@@ -1,6 +1,7 @@
 """
 方阵
 """
+import random
 class FMatrix(object):
     ######### Begin #########
 
@@ -12,18 +13,36 @@ class FMatrix(object):
 
 
     ########## End ##########
-    
 
-    def transpose(self):
-        cols = [list(row) for row in zip(*self.data)]
-        return FMatrix(self.n, cols)
+    def __str__(self):
+        tmpStr = ''
+        for i in range(self.n):
+            row_str = map(lambda x: str(x), self.data[i])
+            tmpStr += '\t'.join(row_str) + '\n'
+        return tmpStr
 
 
-if __name__ == '__main__':
-    A = [[1, 2, 3], [4, 6, 7], [0, 0, 1]]
-    B = [[1, 2, 3], [4, 6, 7], [1, 0, 0]]
+def test_ans():
+    random.seed(88)
+    for count in range(4):
+        n = random.randint(2, 5)
+        data_A = [[random.randint(-100, 100) for i in range(n)] for j in range(n)]
+        data_B = [[random.randint(-100, 100) for i in range(n)] for j in range(n)]
+        A = FMatrix(n, data_A)
+        B = FMatrix(n, data_B)
+        print(A.transpose())
+        print(B.transpose())
+        print(A+B)
+        print(A-B)
+        print(A*B)
+    data_A = [[random.randint(-100, 100) for i in range(3)] for j in range(3)]
+    data_B = [[random.randint(-100, 100) for i in range(2)] for j in range(2)]
+    A = FMatrix(3, data_A)
+    B = FMatrix(2, data_B)
+    print(A.transpose())
+    print(B.transpose())
+    print(A + B)
+    print(A - B)
+    print(A * B)
 
-    C = FMatrix(3,A)
-    D = FMatrix(3,B)
-    E = C+D
-    print(C.transpose())
+test_ans()
--- CollegeComputing/FMatrix/FMatrix_ans.py
+++ CollegeComputing/FMatrix/FMatrix_ans.py
@@ -1,6 +1,7 @@
 """
--fang zen
+方阵
 """
+import random
 
 
 class FMatrix(object):
@@ -16,7 +17,7 @@ class FMatrix(object):
                 for j in range(0, self.n):
                     row.append(self.data[i][j] + other.data[i][j])
                 result.append(row)
-            return FMatrix(self.n,result)
+            return FMatrix(self.n, result)
         else:
             return "Wrong Computed!"
 
@@ -42,11 +43,10 @@ class FMatrix(object):
                 for j in range(self.n):
                     num = 0
                     for n in range(self.n):
-                        num += self.data[i][n]*o_cols[j][n]
-
+                        num += self.data[i][n] * o_cols[j][n]
                     row.append(num)
                 result.append(row)
-            return FMatrix(self.n,result)
+            return FMatrix(self.n, result)
         else:
             return "Wrong Computed!"
 
@@ -62,11 +62,27 @@ class FMatrix(object):
         return FMatrix(self.n, cols)
 
 
-if __name__ == '__main__':
-    A = [[1, 2, 3], [4, 6, 7], [0, 0, 1]]
-    B = [[1, 2, 3], [4, 6, 7], [1, 0, 0]]
+def test_ans():
+    random.seed(88)
+    for count in range(4):
+        n = random.randint(2, 5)
+        data_A = [[random.randint(-100, 100) for i in range(n)] for j in range(n)]
+        data_B = [[random.randint(-100, 100) for i in range(n)] for j in range(n)]
+        A = FMatrix(n, data_A)
+        B = FMatrix(n, data_B)
+        print(A.transpose())
+        print(B.transpose())
+        print(A+B)
+        print(A-B)
+        print(A*B)
+    data_A = [[random.randint(-100, 100) for i in range(3)] for j in range(3)]
+    data_B = [[random.randint(-100, 100) for i in range(2)] for j in range(2)]
+    A = FMatrix(3, data_A)
+    B = FMatrix(2, data_B)
+    print(A.transpose())
+    print(B.transpose())
+    print(A + B)
+    print(A - B)
+    print(A * B)
 
-    C = FMatrix(3,A)
-    D = FMatrix(3,B)
-    E = C+D
-    print(C.transpose())
+test_ans()
